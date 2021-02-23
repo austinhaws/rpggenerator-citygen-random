805e13cbb9a8f15b8e31f087f89e78dac2f18e95
Austin Haws <ahaws@utah.gov>
2021-02-23 15:09:54 -0700

    Add more exports

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const randomMinMaxInt = require('./src/randomMinMaxInt');
 const randomRatio = require('./src/randomRatio');
 const shuffleArray = require('./src/shuffleArray');
 const randomPercentile = require('./src/randomPercentile');
+const createTestRoll = require('./src/test/createTestRoll');
+const mockRolls = require('./src/test/mockRolls');
+const mockRollsAsync = require('./src/test/mockRollsAsync');
+const mockShuffle = require('./src/test/mockShuffle');
+const mockShuffleAsync = require('./src/test/mockShuffleAsync');
 
 exports.randomDice = randomDice.randomDice;
 exports.randomMinMaxFloat = randomMinMaxFloat.randomMinMaxFloat;
@@ -11,3 +16,9 @@ exports.randomMinMaxInt = randomMinMaxInt.randomMinMaxInt;
 exports.randomPercentile = randomPercentile.randomPercentile;
 exports.randomRatio = randomRatio.randomRatio;
 exports.shuffleArray = shuffleArray.shuffleArray;
+
+exports.createTestRoll = createTestRoll;
+exports.mockShuffle = mockShuffle;
+exports.mockShuffleAsync = mockShuffleAsync;
+exports.mockRolls = mockRolls;
+exports.mockRollsAsync = mockRollsAsync;
